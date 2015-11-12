f43785dae5814cfb47d8c4830fa05aa801de34c3
jmahoney13 <jwmahoney13@gmail.com>
2015-11-12 19:08:39 +0000

    new browser functionality up to radio buttons

--- BeagleBone/groundstation.js
+++ BeagleBone/groundstation.js
@@ -24,9 +24,10 @@ function onSerial(x) {
 }
 
 // Create a variable called key, which refers to P9_14
-var key = "P9_14";
+var led = "P9_14";
+
 // Initialize the led as an OUTPUT
-b.pinMode(key, b.OUTPUT);
+b.pinMode(led, b.OUTPUT);
 
 // Initialize the server on port 8888
 var server = http.createServer(function (req, res) {
@@ -63,19 +64,38 @@ var io = require('socket.io').listen(server);
 // When communication is established
 io.on('connection', function (socket) {
     socket.on('changeState', handleChangeState);
+    socket.on('handleRadioButtons', handleMotorSpeed);
 });
 
 // Change led state when a button is pressed
 function handleChangeState(data) {
     var newData = JSON.parse(data);
-    console.log("LED = " + newData.state);
-    // turns the LED ON or OFF
-	 if (newData.state == '1') {
+    console.log("MOTOR = " + newData.state);
+    
+    // turns the motor forward (1) reverse (2) 
+	if (newData.state == '1') {
         b.serialWrite(port, [0x01]);
     }
     else if (newData.state == '2') {
         b.serialWrite(port, [0x02]);
     }
+    
+    //led on/off 
+    if (newData.state == '3') {
+        console.log("LED = " + 1);
+        b.digitalWrite(led, 1);
+    }
+    else if (newData.state == '4') {
+        console.log("LED = " + 0);
+        b.digitalWrite(led, 0);
+    }
+}
+
+function handleMotorSpeed(data) {
+    var newData = JSON.parse(data);
+    console.log("Speed = " + newData.state);
+    
+    
 }
 
 // Displaying a console message for user feedback
